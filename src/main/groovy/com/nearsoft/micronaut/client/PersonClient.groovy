c544ea76ed2fb9e726a195fcb3a8df4366d728a9
Omar Bautista <obautista@skytouchtechnology.com>
2018-11-07 18:40:44 -0700

    Fix client failure by adding @Body to the create/update methods.

--- src/main/groovy/com/nearsoft/micronaut/client/PersonClient.groovy
+++ src/main/groovy/com/nearsoft/micronaut/client/PersonClient.groovy
@@ -1,20 +1,13 @@
 package com.nearsoft.micronaut.client
 
 import com.nearsoft.micronaut.api.PersonApi
-import com.nearsoft.micronaut.domain.Person
-import io.micronaut.http.annotation.Post
-import io.micronaut.http.annotation.Put
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Delete
 import io.micronaut.http.client.annotation.Client
 
 @Client("/people")
 interface PersonClient extends PersonApi {
-
-    // Without this 2 methods the test fails cause send a malformed request
-    // TODO: need to check why can't just simply call the create(Person) method
-    @Post("/")
-    Person create(String name, String lastName, int age, String phone)
-
-    @Put("/")
-    Person update(Long id, String name, String lastName, int age, String phone)
-
+    @Delete("/{id}")
+    @Override
+    HttpResponse delete(Long id)
 }
