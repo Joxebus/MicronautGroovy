f82c6ac7942e973b3245b7851929c3c1bfe3e2c6
Omar Bautista <joxebus@gmail.com>
2020-09-16 17:15:04 -0500

    Fix compilation errors on PersonController

--- src/main/groovy/io/github/joxebus/micronaut/controller/PersonController.groovy
+++ src/main/groovy/io/github/joxebus/micronaut/controller/PersonController.groovy
@@ -6,6 +6,7 @@ import groovy.transform.CompileStatic
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Delete
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.Put
@@ -20,31 +21,27 @@ class PersonController {
         this.personService = personService
     }
 
-    @Override
     @Post("/")
     Person create(@Body Person person){
         personService.save(person)
     }
 
-    @Override
     @Put("/")
     Person update(@Body Person person){
         personService.save(person)
     }
 
-    @Override
     @Get("/")
     List<Person> list() {
         personService.findAll()
     }
 
-    @Override
     @Get("/{id}")
     Person show(Long id) {
         personService.find(id)
     }
 
-    @Override
+    @Delete("/{id}")
     HttpResponse delete(Long id) {
         personService.delete(id)
         HttpResponse.ok()
