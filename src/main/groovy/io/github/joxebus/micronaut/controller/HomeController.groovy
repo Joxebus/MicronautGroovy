e6b358d02d6b49d572af08899c6d63dd89cf93e2
Omar Bautista <joxebus@gmail.com>
2020-09-16 22:25:55 -0500

    Enable Security with OAuth2 Google (#4)
    * Enable Security with OAuth2 Google
    
    - Added dependencies to build.gradle
    - Added new application-heroku configuration to read the variables
    - Configure HomeController to show as Anonymous
    - Modify header to have the login/logout url
    - Home view now will show anonymous or the email of the logged user
    
    * Update README with badge for branch

--- src/main/groovy/io/github/joxebus/micronaut/controller/HomeController.groovy
+++ src/main/groovy/io/github/joxebus/micronaut/controller/HomeController.groovy
@@ -2,12 +2,15 @@ package io.github.joxebus.micronaut.controller
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
 import io.micronaut.views.ModelAndView
 
 @Controller("/")
 class HomeController {
 
 
+    @Secured(SecurityRule.IS_ANONYMOUS)
     @Get("/")
     ModelAndView home(){
         def model = [message: "Hello from micronaut"]
